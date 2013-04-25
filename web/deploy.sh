cdc3053b0755a9c0503a37b321cfba2f25c67758
danmux <danm@nadx.co.uk>
2013-04-25 09:31:07 +0100

    adding first cut of the site

---
+++ web/deploy.sh
@@ -0,0 +1,15 @@
+
+
+echo '>>>>>>>>>>>>>>>>>>>>>>>>'
+echo '>>> Any DEBUG stuff  >>>'
+echo '>>>>>>>>>>>>>>>>>>>>>>>>'
+echo	
+echo	
+find . | xargs grep DEBUG 
+
+echo
+echo '>>>>>>>>>>>>>>>>>>>>>>>>'
+echo '>>>>>>>>>>>>>>>>>>>>>>>>'
+
+
+/usr/local/bin/appcfg.py  update  .
---
+++ web/start.sh
@@ -0,0 +1,3 @@
+#/usr/local/bin/dev_appserver.py  -c .
+
+/usr/local/bin/dev_appserver.py --log_level debug  --host=localhost --port=8080 --admin_host=localhost --admin_port=8081 .
